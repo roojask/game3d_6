e971f90e5a129593a056ae8ec5b22bd4a87988f4
roojask <thaninrat.l@kkumail.com>
2025-09-20 17:14:08 +0700

    r

--- github/game3d_lab6/asset/demo.gd
+++
@@ -1,7 +0,0 @@
-extends Node3D
-
-func _ready():
-	$Walking/AnimationPlayer.play("Melee-Library--OLD/run")
-	$Walking2/AnimationPlayer.play("Melee-Library--OLD/walk")
-	$Walking3/AnimationPlayer.play("Locomotion-Library/kick1")
-	$Walking4/AnimationPlayer.play("Melee-Library--OLD/HeavyATK3")
